deb0542c2251feb32538550ecc4ee9528935f4c5
github username <github email address>
2018-07-24 13:37:52 -0400

    Done.

---
+++ db/seed.rb
@@ -0,0 +1,2 @@
+Song.create(title: "Hola", released: true, release_year: 1995, artist_name: "andres", genre: "balada")
+Song.create(title: "Aloha", released: true, release_year: 1996, artist_name: "andres", genre: "regue")
